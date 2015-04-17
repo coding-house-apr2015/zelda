911eb6ea9dca51fa9d3a563e1d4b0d9dcd10f730
Chyld Medford <chyld.medford@gmail.com>
2015-04-16 17:18:40 -0700

    create user

--- client/index.js
+++ client/index.js
@@ -1,1 +1,31 @@
 'use strict';
+
+var root, users, me;
+
+$(document).ready(init);
+
+function init(){
+  root = new Firebase('https://zelda-chyld.firebaseio.com/');
+  users = root.child('users');
+  $('#create-user').click(createUser);
+  users.on('child_added', userAdded);
+}
+
+function userAdded(snapshot){
+}
+
+function createUser(){
+  var email = $('#email').val();
+  var password = $('#password').val();
+
+  root.createUser({
+    email    : email,
+    password : password
+  }, function(error, userData) {
+    if (error) {
+      console.log("Error creating user:", error);
+    } else {
+      console.log("Successfully created user account with uid:", userData.uid);
+    }
+  });
+}
