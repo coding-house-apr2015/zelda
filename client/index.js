68e195b71052e68e50264f16751b7334ecbf2bdc
Chyld Medford <chyld.medford@gmail.com>
2015-04-16 19:46:48 -0700

    avatar

--- client/index.js
+++ client/index.js
@@ -1,17 +1,57 @@
+/* global Firebase:true */
+
 'use strict';
 
-var root, users, me;
+var root, characters;
 
 $(document).ready(init);
 
 function init(){
   root = new Firebase('https://zelda-chyld.firebaseio.com/');
-  users = root.child('users');
+  characters = root.child('characters');
   $('#create-user').click(createUser);
-  users.on('child_added', userAdded);
+  $('#login-user').click(loginUser);
+  characters.on('child_added', characterAdded);
+  $('#create-character').click(createCharacter);
 }
 
-function userAdded(snapshot){
+function createCharacter(){
+  var handle = $('#handle').val();
+  var avatar = $('#avatar').val();
+  var uid = root.getAuth().uid;
+
+  characters.push({
+    handle: handle,
+    avatar: avatar,
+    uid: uid
+  });
+}
+
+function characterAdded(snapshot){
+  var character = snapshot.val();
+  var myUid = root.getAuth().uid;
+  var active = '';
+
+  if(myUid === character.uid){
+    active = 'active';
+  }
+
+  var tr = '<tr class="'+active+'"><td>'+character.handle+'</td><td><img src="'+character.avatar+'"></td></tr>';
+  $('#characters > tbody').append(tr);
+}
+
+function loginUser(){
+  var email = $('#email').val();
+  var password = $('#password').val();
+
+  root.authWithPassword({
+    email    : email,
+    password : password
+  }, function(error){
+    if(error){
+      console.log('Error logging in:', error);
+    }
+  });
 }
 
 function createUser(){
@@ -21,11 +61,9 @@ function createUser(){
   root.createUser({
     email    : email,
     password : password
-  }, function(error, userData) {
-    if (error) {
-      console.log("Error creating user:", error);
-    } else {
-      console.log("Successfully created user account with uid:", userData.uid);
+  }, function(error){
+    if(error){
+      console.log('Error creating user:', error);
     }
   });
 }
